5ee3534f6fec3da45e48ff63795aafed6d925b15
Shuu-55 <112682510+Shuu-55@users.noreply.github.com>
2023-10-23 17:19:10 +0530

    Add files via upload

---
+++ devops-cicd-b2-lab1/src/main/java/MyCalc.java
@@ -0,0 +1,17 @@
+public class MyCalc{
+	
+	public int sum(int a, int b) {
+		return(a+b);
+	}
+	public int diff(int a, int b) {
+		return(a-b);
+	}
+	
+	public static void main(String[] args) {
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is-"+calc.sum(10,40));
+		System.out.println("Diff is-"+calc.diff(10,40));
+
+	}
+
+}
